330a4b3ac29abf6b93cc2dcb31d52531479cb5d1
codeKonami <thomas.foricher@gmail.com>
2015-10-04 11:19:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Genuine 1.0.1
+
+This library should be installed globally like so
+
+```bash
+npm install -g genuine
+```
+
+Then use it like so
+
+```bash
+#genuine create <name of the project>
+genuine create foo
+#Creating project foo...
+#Project foo created.
+#The file genuine.json has been added to the project.
+
+```
+
+By default, it will generate a noob project and integrate a genuine.json file.
+
+You can though precise any github project you'd like to copy like so.
+
+```bash
+#genuine create <name of the project>
+genuine create bar codeKonami/Genuine
+#Creating project bar...
+#Project bar created.
+#The file genuine.json has been added to the project.
+```
---
+++ bin/genuine.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+'use strict'
+var fs = require('fs');
+var userArgs = process.argv.slice(2);
+var exec = require('child_process').exec;
+var pattern = /^[a-z\_\-]+$/;
+var repoSlug = "codeKonami/noob.js";
+var genuine = {
+  create : function(){
+    console.log("Creating project " + projectName + "...");
+    var child = exec('git clone https://github.com/'+ repoSlug +'.git ' + projectName, function(err, stdout, stderr) {
+      if(err){
+        console.log(stderr);
+        return;
+      }
+      console.log("Project " + projectName + " created.");
+      var child = exec('rm -Rf ' + projectName + '/.git', function(err, stdout, stderr) {
+        fs.writeFile(projectName + '/genuine.json', JSON.stringify({"app_name" : projectName}, null, 4), function(err) {
+          if(err) {
+            return console.log(err);
+          }
+          console.log("The file genuine.json has been added to the project.");
+        });
+      });
+    });
+  },
+  shows : function(){
+    console.log("Usage: genuine [action] <name of the project>");
+    console.log("\n");
+    console.log("Actions:");
+    console.log("  create         create a new project");
+    console.log("  shows help     show this page");
+  }
+}
+
+if(userArgs.length < 2){
+  console.log("genuine needs two arguments. Type *genuine shows help* for more information");
+} else if(!pattern.test(userArgs[0])||!pattern.test(userArgs[1])){
+  console.log("The action and the name of the project need to be lowercase without numbers. '-' and '_' are ok.");
+} else if(typeof genuine[userArgs[0]] == 'undefined'){
+  console.log("The action "+action+" isn't valid. Type *genuine shows help* for more information");
+} else {
+  // It seems everything is fine
+  var action = userArgs[0];
+  var projectName = userArgs[1];
+  if(typeof userArgs[2] != 'undefined'){
+    repoSlug = userArgs[2];
+  }
+  genuine[action]();
+}
